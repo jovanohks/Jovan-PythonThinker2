93e3ac50f56fbc6cd07b1b3a17dc35f82a157a82
jovanohks <jovanoh400@gmail.com>
2025-01-17 09:47:35 +0000

    Jan 17, 2025, 9:47 AM

--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -4,7 +4,7 @@
 # 1. from 0 - 20
 # 2. from 1 to 30
 # 3. from 2 to 24 (in even numbers)
-for i in range(21):
-    print(i)
-for i in range(1,30):
+# for i in range(21):
+#     print(i)
+for i in range(1,31):
     print(i)
