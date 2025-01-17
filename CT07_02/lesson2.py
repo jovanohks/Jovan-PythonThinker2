a557948142e535107637a2535e8ac369fb3559f3
jovanohks <jovanoh400@gmail.com>
2025-01-17 09:45:30 +0000

    Jan 17, 2025, 9:45 AM

--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -1,2 +1,6 @@
-print("Hello from lesson 2")
-
+## Recap 1: For Loop
+# Task: write a separate 'for' loop to print the following
+# numbers:**
+# 1. from 0 - 20
+# 2. from 1 to 30
+# 3. from 2 to 24 (in even numbers)
