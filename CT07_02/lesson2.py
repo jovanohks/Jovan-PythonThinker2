272885e342c22d6dff029e2188ef901d4c9dcbe5
jovanohks <jovanoh400@gmail.com>
2025-01-17 09:53:25 +0000

    Jan 17, 2025, 9:53 AM

--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -8,5 +8,12 @@
 #     print(i)
 # for i in range(1,31):
 #     print(i)
-for i in range(2,25,2):
-    print(i)
+# for i in range(2,25,2):
+    # print(i)
+
+## Task 1: Introduction to while loop
+# **Task: Using a separate 'while' loop, print each of the
+# following:**
+# 1. from 0 to 20
+# 2. from 1 to 30
+# 3. from 2 to 24
