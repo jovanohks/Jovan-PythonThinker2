ab120aba52952e5053850b6ee886fcede2606d43
jovanohks <jovanoh400@gmail.com>
2025-01-24 09:51:44 +0000

    Jan 24, 2025, 9:51 AM

--- CT07_03/lesson3.py
+++ CT07_03/lesson3.py
@@ -7,14 +7,14 @@ import time
 # 2. Using the while loop, ask the user the riddle and
 #    check the answer
 # 3. While answer is not correct, repeat the question
-riddleqans = ["what is 1+1?", "2"]
-ans = False
-while ans != True:
-    x = input(riddleqans[0])
-    if x == riddleqans[1]:
-        break
-    else:
-        print("u are wrong.")
+# # riddleqans = ["what is 1+1?", "2"]
+# # ans = False
+# # while ans != True:
+# #     x = input(riddleqans[0])
+# #     if x == riddleqans[1]:
+# #         break
+# #     else:
+# #         print("u are wrong.")
 
 
 # riddle_ans = "phone"
