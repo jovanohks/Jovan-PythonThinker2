57b0257e6d16d7f4efc3c8f91069f394212121ab
jovanohks <jovanoh400@gmail.com>
2025-02-14 10:44:04 +0000

    Feb 14, 2025, 10:44 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -112,5 +112,5 @@ heightlist = [160, 165, 158, 170, 162, 168, 159, 172, 164, 166]
 
 "xxx is the tallest at yyy cm"
 "www is the shortest at zzz cm"
-print(namelist[heightlist.index(max(heightlist))]+" is the tallest at ")
+print(namelist[heightlist.index(max(heightlist))]+" is the tallest at "heightlist[heightlist.index(max(heightlist))])
 print(namelist[heightlist.index(min(heightlist))])
