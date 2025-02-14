8c25892f2718cc98b55ef7d937ebbaa7d3ec7724
jovanohks <jovanoh400@gmail.com>
2025-02-14 11:00:27 +0000

    Feb 14, 2025, 11:00 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -160,7 +160,8 @@ else:
 if powers[pokemons.index(p1)] == powers[pokemons.index(p2)]:
     print(f"{p1} ties with {p2}!")
 else:
-    winner, loser = p1, p2 if powers[pokemons.index(p1)] > powers[pokemons.index(p2)] else p2, p1
+    winner = p1 if powers[pokemons.index(p1)] > powers[pokemons.index(p2)] else p2
+    winner = p1 if powers[pokemons.index(p1)] > powers[pokemons.index(p2)] else p2
     print(f"{winner} wins!")
     print(f"{loser} loses!")
     
