c1e31eb89a2004357ead5b6456653a29dff441f9
jovanohks <jovanoh400@gmail.com>
2025-02-14 10:56:20 +0000

    Feb 14, 2025, 10:56 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -151,7 +151,8 @@ if powers[pokemons.index(p1)]-powers[pokemons.index(p2)] >0:
 elif powers[pokemons.index(p1)]-powers[pokemons.index(p2)] <0:
     print(f"{p1} loses!")
     print(f"{p2} wins!")
-elif powers[pokemons.index(p1)]-powers[pokemons.index(p2)] == 0:
+# elif powers[pokemons.index(p1)]-powers[pokemons.index(p2)] == 0:
+else:
     print(f"{p1} ties with {p2}!")
 
 # 
