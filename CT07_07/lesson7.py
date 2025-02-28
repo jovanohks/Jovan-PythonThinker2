22323d0581c0a91b83ef819c03b403c996c0965c
jovanohks <jovanoh400@gmail.com>
2025-02-28 10:06:36 +0000

    Feb 28, 2025, 10:06 AM

--- CT07_07/lesson7.py
+++ CT07_07/lesson7.py
@@ -63,4 +63,4 @@ index = 3
 
 # 1. Use slicing to split the list at the provided index.
 # 2. Print the resulting SUBLISTS. I want 2 lists! i.e a left and a right list
-print(fruits[1:3],fruits[3:])
+print(fruits[0:3],fruits[3:])
