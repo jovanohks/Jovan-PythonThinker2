08aab832c8b5132ffa0df0982e6e48f5bdc83e3f
jovanohks <jovanoh400@gmail.com>
2025-02-28 09:43:46 +0000

    Feb 28, 2025, 9:43 AM

--- CT07_07/lesson7.py
+++ CT07_07/lesson7.py
@@ -22,6 +22,8 @@ student3 = ["Sylvia", 87894032, "Dance"]
 student.append(student1)
 student.append(student2)
 student.append(student3)
-for i in student:
+for i in student: # what is the "i" variable used for
     name , number , cca = something 
-    print
+    print(name)
+    print(number)
+    print(cca)
