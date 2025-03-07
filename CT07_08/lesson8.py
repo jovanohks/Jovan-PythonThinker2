f09f135b75c8707700588250d9a0a2261c1020bc
jovanohks <jovanoh400@gmail.com>
2025-03-07 09:43:47 +0000

    Mar 7, 2025, 9:43 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -4,9 +4,10 @@ print("Hello from lesson 8")
 #Q1
 order = []
 while not userwant =="end":
-    
+    order.append(userwant)
     userwant= input("What would you like to order? ")
-    
+else:
+    print(order)
 
 
 
