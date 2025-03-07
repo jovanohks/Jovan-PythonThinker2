046655e1ed8e0513af74231e98abf3affa9e163d
jovanohks <jovanoh400@gmail.com>
2025-03-07 09:51:43 +0000

    Mar 7, 2025, 9:51 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -12,7 +12,7 @@ while True:
         order.append(userwant) #append what user has ordered to the stuff that user want
 print("You have ordered the following:")
 for items in order:
-    print(str(order.index(items)+1)+". "+items) #print the 
+    print(str(order.index(items)+1)+". "+items) #print the index of items plus 1 , and tell user what 
 
 
 
