a5761cc3849375683b4d6b9051238872da2df9d4
jovanohks <jovanoh400@gmail.com>
2025-03-07 09:58:56 +0000

    Mar 7, 2025, 9:58 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -9,7 +9,7 @@ while health >=1:
     minushealth = random.randint(1,15)
     health = health - minushealth
     print("After fighting monsters, his health is now: "+str(health))
-    
+print("hero has died.")
 
 
 
