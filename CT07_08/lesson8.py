04922c307940826b08e3cf16770cf0a7f92f9ffb
jovanohks <jovanoh400@gmail.com>
2025-03-07 09:53:05 +0000

    Mar 7, 2025, 9:53 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -2,17 +2,17 @@ print("Hello from lesson 8")
 
 
 #Q1
-order = [] # list  to store food user wants
-userwant="" #a variable to store the
-while True:
-    userwant= input("What would you like to order? ") #ask user for what food to add to order
-    if userwant == "end":
-        break #if user type end, end the program
-    else:
-        order.append(userwant) #append what user has ordered to the stuff that user want
-print("You have ordered the following:")
-for items in order:
-    print(str(order.index(items)+1)+". "+items) #print the index of items plus 1 since index start from 0 , and tell user what he has ordered
+# order = [] # list  to store food user wants
+# userwant="" #a variable to store the
+# while True:
+#     userwant= input("What would you like to order? ") #ask user for what food to add to order
+#     if userwant == "end":
+#         break #if user type end, end the program
+#     else:
+#         order.append(userwant) #append what user has ordered to the stuff that user want
+# print("You have ordered the following:")
+# for items in order:
+#     print(str(order.index(items)+1)+". "+items) #print the index of items plus 1 since index start from 0 , and tell user what he has ordered
 
 
 
