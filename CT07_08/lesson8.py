2977774508bc690159e601ad74f768400b5c61da
jovanohks <jovanoh400@gmail.com>
2025-03-07 10:01:35 +0000

    Mar 7, 2025, 10:01 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -1,36 +1,36 @@
 print("Hello from lesson 8")
 
 #Q1
-import random
-health=100
-num_of_battles = 0
-print("Hero starts on his adventure with Health: 100")
-while True:
-    minushealth = random.randint(1,15)
-    health = health - minushealth
-    if health <=0:
-        break
-    else:
-        print(f"After fighting monsters, his Health is now: {health}")
-        num_of_battles +=1
-print(f"He fought {num_of_battles} battles, and died.")
+# import random
+# health=100
+# num_of_battles = 0
+# print("Hero starts on his adventure with Health: 100")
+# while True:
+#     minushealth = random.randint(1,15)
+#     health = health - minushealth
+#     if health <=0:
+#         break
+#     else:
+#         print(f"After fighting monsters, his Health is now: {health}")
+#         num_of_battles +=1
+# print(f"He fought {num_of_battles} battles, and died.")
 
 
 
 
 
 #Q2
-# order = [] # list  to store food user wants
-# userwant="" #a variable to store the
-# while True:
-#     userwant= input("What would you like to order? ") #ask user for what food to add to order
-#     if userwant == "end":
-#         break #if user type end, end the program
-#     else:
-#         order.append(userwant) #append what user has ordered to the stuff that user want
-# print("You have ordered the following:")
-# for items in order:
-#     print(str(order.index(items)+1)+". "+items) #print the index of items plus 1 since index start from 0 , and tell user what he has ordered
+order = [] # list  to store food user wants
+userwant="" #a variable to store the
+while True:
+    userwant= input("What would you like to order? ") #ask user for what food to add to order
+    if userwant == "end":
+        break #if user type end, end the program
+    else:
+        order.append(userwant) #append what user has ordered to the stuff that user want
+print("You have ordered the following:")
+for items in order:
+    print(str(order.index(items)+1)+". "+items) #print the index of items plus 1 since index start from 0 , and tell user what he has ordered
 
 
 
