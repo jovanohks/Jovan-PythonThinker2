23b91d79ed942453f079fb3b68ecb55d73e146b1
jovanohks <jovanoh400@gmail.com>
2025-03-14 09:34:06 +0000

    Mar 14, 2025, 9:34 AM

---
+++ CT07_08/assessment.py
@@ -0,0 +1,43 @@
+print("Hello from lesson 8")
+
+#Q1
+import random
+health=100
+num_of_battles = 0
+print("Hero starts on his adventure with Health: 100")
+while True:
+    minushealth = random.randint(1,15)
+    health = health - minushealth
+    if health <=0:
+        break
+    else:
+        print(f"After fighting monsters, his Health is now: {health}")
+        num_of_battles +=1
+print(f"He fought {num_of_battles} battles, and died.")
+
+
+
+
+
+#Q2
+order = [] # list  to store food user wants
+userwant="" #a variable to store the food that user wants to add to order
+numitems = 1 #count the number of food
+while True:
+    userwant= input("What would you like to order? ") #ask user for what food to add to order
+    if userwant == "end":
+        break #if user type end, end the program
+    else:
+        order.append(userwant) #append what user has ordered to the stuff that user want
+print("You have ordered the following:")
+for items in order:
+    print(f"{numitems}. {items}") #add number of food , and tell user what he has ordered
+    numitems +=1 # increment number of object to print for next item
+    
+
+
+
+
+
+
+
