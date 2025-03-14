743ec15263933e705609ad4a705406325e617f41
jovanohks <jovanoh400@gmail.com>
2025-03-14 10:45:41 +0000

    Mar 14, 2025, 10:45 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -151,4 +151,4 @@
 # Example:
 # Input: ['Computers', 'empower', 'our', 'modern', 'world', 'with', 'their', 'digital', 'brains.']
 # Output: "Computers empower our modern world with their digital brains."
-
+sentence = ['Computers', 'empower', 'our', 'modern', 'world', 'with', 'their', 'digital', 'brains.']
