2f3983a30479854a32f847f605a58a09482b3ecb
jovanohks <jovanoh400@gmail.com>
2025-03-14 09:51:18 +0000

    Mar 14, 2025, 9:51 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -9,10 +9,13 @@
 list1 = [3, 2, 1]
 list2 = [6, 5, 5]
 list3 = [9, 8, 7]
-
+list5=[]
+list6=[]
 # 1. Merge the 3 lists and remove duplicates.
 # 2. Sort the resulting list.
 # 3. Split the list into 2 sorted halves.
 # 4. Print the halves.
 list4=sorted(set(list1+list2+list3))
 print(list4)
+list5.append(len(list4//2))
+list6.append(len(list4//2))
