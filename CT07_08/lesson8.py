03c072ecca136ef2669253f1dde902d3279fa4d9
jovanohks <jovanoh400@gmail.com>
2025-03-21 10:08:31 +0000

    Mar 21, 2025, 10:08 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -219,9 +219,9 @@ x = input("enter sentence: ")
 y = x.split(" ")
 z = []
 print(y)
-for i in len(y):
+for i in range(len(y)):
     if y[i][::-1] ==y[i]:
         z.append(y[i])
 print(f"{len(z)} palindrome(s) detected: ")
-for i in len(z):
+for i in range(len(z)):
     print(z[i])
