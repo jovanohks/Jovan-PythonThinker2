e6539c45099380775ae79dae1e22082c3c0b8056
jovanohks <jovanoh400@gmail.com>
2025-03-21 10:37:12 +0000

    Mar 21, 2025, 10:37 AM

--- CT07_09/lesson9.py
+++ CT07_09/lesson9.py
@@ -2,11 +2,12 @@ import turtle
 window = turtle.Screen()
 window.setup(600,400)
 window.bgcolor("forestgreen")
-t=turtle.Turtle()
-t.seth(0)
-t.pendown
-for _ in range(4):
-    t.forward(100)
-    t.left(90)
-t.penup()
+# t=turtle.Turtle()
+# t.seth(0)
+# t.pendown
+# for _ in range(4):
+#     t.forward(100)
+#     t.left(90)
+# t.penup()
+
 window.mainloop()
