81f9769fa3ddaedb29e71f1881998003aa2ab9a6
jovanohks <jovanoh400@gmail.com>
2025-03-21 10:37:22 +0000

    Mar 21, 2025, 10:37 AM

--- CT07_09/lesson9.py
+++ CT07_09/lesson9.py
@@ -10,4 +10,27 @@ window.bgcolor("forestgreen")
 #     t.left(90)
 # t.penup()
 
+# **Task 1b**: Drawing the finish line
+# Adding on to your previous answer, create a line of black
+# squares at y = 250 by creating a black square turtle object
+# and stamping it from x = -300 to x = 300 at 25 pixel-
+# intervals
+
+# 1. Create a turtle object named 'pen'
+# 2. Lift the pen up
+# 3. Using '.shape()', set the turtle object's shape to "square"
+# 4. Using '.sety()', set turtle object's y position to 250
+# 5. Using a 'for i in range()' loop, use the 'i' variable to
+#    create a stamp of 'pen' turtle object from x = -300 to
+#    x = 300 at 25-pixel intervals:
+#         a. Set 'pen' turtle's x coordinate using '.setx()'
+#         b. Stamp a copy of 'pen' turtle at its current
+#            position using '.stamp()'
+
+# Tip: Use '.mainloop()' at the end of your program to keep the
+# window open
+
+# You should see this when you run the program! (Refer to slides)
+
+
 window.mainloop()
