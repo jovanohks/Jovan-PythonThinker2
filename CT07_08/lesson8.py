a6222ea7bc17af8e1b7fc9b5091196f499529ca0
jovanohks <jovanoh400@gmail.com>
2025-03-21 09:55:15 +0000

    Mar 21, 2025, 9:55 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -188,11 +188,7 @@
 # Input: "radar"
 # Output: True
 x = input("what is input?")
-
-if x[::-1] == x :
-    print("true")
-else:
-    print("false")
+print(x[::-1] == x)
 
 
 ## Task 7: Checking user input for Palindrome
