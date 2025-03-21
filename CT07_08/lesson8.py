dfbe28e4e22355111e525fc28c3a78605960c508
jovanohks <jovanoh400@gmail.com>
2025-03-21 10:10:12 +0000

    Mar 21, 2025, 10:10 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -219,7 +219,7 @@ x = input("enter sentence: ")
 y = x.split(" ")
 z = []
 print(y)
-for i in range(len(y)):
+for i in y:
     if y[i][::-1] ==y[i]:
         z.append(y[i])
 print(f"{len(z)} palindrome(s) detected: ")
