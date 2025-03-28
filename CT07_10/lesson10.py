de451afd2ab2481af2a4b28614896527aa78f8ce
jovanohks <jovanoh400@gmail.com>
2025-03-28 10:52:17 +0000

    Mar 28, 2025, 10:52 AM

--- CT07_10/lesson10.py
+++ CT07_10/lesson10.py
@@ -1,2 +1,21 @@
-print("Hello from lesson 9")
+## Task 1: Function without parameter (w/o turtle)
+# You are required to print the same "Motion Detected" phrase
+# multiple times as part of a motion detecting program that you
+# are creating.
 
+# Create an 'alert()' function that will print "Motion Detected"
+# whenever the function is called.
+
+# **Example**
+# Input:
+#     alert()
+# Output:
+#     Motion Detected
+
+## Task 2: Function without parameter (w turtle)
+# Using the 'turtle' library, create a 'square()' function that
+# draws a 20x20 square at the turtle object's current position
+# whenever the function is called.
+
+# By calling the 'square()' function, draw a square anywhere
+# within the turtle window.
