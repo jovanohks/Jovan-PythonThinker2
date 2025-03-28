a1571888adba8ea83a5b12f5f013eb80e1251597
jovanohks <jovanoh400@gmail.com>
2025-03-28 10:57:22 +0000

    Mar 28, 2025, 10:57 AM

--- CT07_10/lesson10.py
+++ CT07_10/lesson10.py
@@ -33,4 +33,46 @@ def square():
         t.forward(20)
         t.right(90)
 square()
+
+## Task 3: Function with parameter (w/o turtle)
+# Write a Python function 'multiply()', that takes 2 parameters,
+# a and b, and prints the product of these 2 numbers.
+
+# **Example**
+# Input:
+#     multiply(3,5)
+# Output:
+#     15
+
+## Task 4: Function with parameter (w turtle)
+# Using the 'turtle' library, create a function 'drawSquare()',
+# with 2 parameters, x and y, and draw a 20x20 square at the
+# coordinate (x, y).
+
+# You may use the following steps as a guide:
+# 1. Import 'turtle' library
+# 2. Create a turtle object using 'turtle.Turtle()'
+# 3. Define a function 'drawSqare()' with 2 parameters, x and y
+#         a. Lift the pen using '.penup()'
+#         b. Reposition the turtle object using '.goto(x, y)'
+#         c. Put the pen down using '.pendown()'
+#         d. Create a 'for' loop to draw a square
+# 4. Test out your program using 'drawSquare(-50, 50)'
+# 5. Use '.mainloop()' to keep the window open
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 window.mainloop()
